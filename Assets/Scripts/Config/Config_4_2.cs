85d2901c0ab5fc72b47cf0d78420a9614f263cdb
CarlierAlex2 <alex.carlier@gmail.com>
2022-01-15 18:26:54 +0100

    CarAgent4 v3

---
+++ Assets/Scripts/Config/Config_4_2.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentConfig_4_2
+{
+    public static bool hasInstance = false;
+    private static AgentConfig_4_1 instance;
+    public static AgentConfig_4_1 Instance
+    {
+        get 
+        {
+            if (!hasInstance)
+            {
+                hasInstance = true;
+                instance = new AgentConfig_4_1();
+            }
+            return instance;
+        }
+    }
+
+    //--
+    public int INDEX_START { get { return 1; } }
+
+
+    //--
+    public float CHECKPOINT_OFFSET = 1f;
+
+
+    //--
+    public float CHECKPOINT_RANGE = 2.5f;
+    public float CHECKPOINT_ANGLE_MAX { get { return 140f; } }
+
+
+    //--
+    public float VELOCITY_MIN { get { return 0.1f; } }
+    public float STEERING_ANGLE { get { return 60f; } }
+}
+
+public class RewardConfig_4_2
+{
+    public static bool hasInstance = false;
+    private static RewardConfig_4_1 instance;
+    public static RewardConfig_4_1 Instance
+    {
+        get 
+        {
+            if (!hasInstance)
+            {
+                hasInstance = true;
+                instance = new RewardConfig_4_1();
+            }
+            return instance;
+        }
+    }
+
+    //--
+    public float TIME { get { return -0.05f; } }
+    public float BREAK { get { return -0.5f; } }
+    public float GOAL { get { return 100f; } }
+
+
+    //--
+    public float WALL_ENTER { get { return -5f; } }
+    public float WALL_STAY { get { return -1f; } }
+
+
+    //--
+    public float CHECKPOINT_RANGE { get { return 5f; } }
+    public float CHECKPOINT_PASS { get { return -5f; } }
+
+
+    //--
+    public float VELOCITY_MIN { get { return -2f; } }
+    public float VELOCITY_FORWARD { get { return 0.1f; } }
+    public float STEERING_ANGLE { get { return -0.1f; } }
+
+}
