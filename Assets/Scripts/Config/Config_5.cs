e14d32a386a429d5c095d1239967beee808c8ef7
CarlierAlex2 <alex.carlier@gmail.com>
2022-01-17 13:40:46 +0100

    CarAgent5 v2 - more observation

--- Assets/Scripts/Config/Config_5.cs
+++ Assets/Scripts/Config/Config_5.cs
@@ -10,9 +10,9 @@ public class ConfigAgent_5_1 : ConfigAgent
         //--
         this.CHECKPOINT_OFFSET = 1f;
         this.CHECKPOINT_RANGE = 2.3f;
-        this.CHECKPOINT_ANGLE_MAX = 140f;
+        this.CHECKPOINT_ANGLE_MAX = 0;
         //--
-        this.VELOCITY_MIN = 0.1f;
+        this.VELOCITY_MIN = 0;
         this.STEERING_ANGLE = 30f;
     }
 }
@@ -21,21 +21,55 @@ public class ConfigReward_5_1 : ConfigReward
 {
     public ConfigReward_5_1() : base()
     {
-        this.TIME = -0.1f;
+        this.TIME = -0.2f;
         this.BREAK = -0.5f;
-        this.GOAL = 100f;
+        this.GOAL = 0;
         //--
         this.WALL_ENTER = -5f;
-        this.WALL_STAY = -1f;
+        this.WALL_STAY = -5f;
         //--
         this.CHECKPOINT_RANGE = 5f;
         this.CHECKPOINT_PASS = -5f;
         //--
-        this.VELOCITY_MIN = -2f;
-        this.VELOCITY_FORWARD = 0.1f;
+        this.VELOCITY_MIN = 0;
+        this.VELOCITY_FORWARD = 0;
         this.STEERING_ANGLE = -0.5f;
     }
 }
 
 //-------------------------------------------------------------------
 
+public class ConfigAgent_5_2 : ConfigAgent
+{
+    public ConfigAgent_5_2() : base()
+    {
+        this.INDEX_START = 1;
+        //--
+        this.CHECKPOINT_OFFSET = 1f;
+        this.CHECKPOINT_RANGE = 2.2f;
+        this.CHECKPOINT_ANGLE_MAX = 0;
+        //--
+        this.VELOCITY_MIN = 0;
+        this.STEERING_ANGLE = 30f;
+    }
+}
+
+public class ConfigReward_5_2 : ConfigReward
+{
+    public ConfigReward_5_2() : base()
+    {
+        this.TIME = -0.2f;
+        this.BREAK = -0.5f;
+        this.GOAL = 0;
+        //--
+        this.WALL_ENTER = -5f;
+        this.WALL_STAY = -5f;
+        //--
+        this.CHECKPOINT_RANGE = 5f;
+        this.CHECKPOINT_PASS = 0;
+        //--
+        this.VELOCITY_MIN = 0;
+        this.VELOCITY_FORWARD = 0;
+        this.STEERING_ANGLE = -0.1f;
+    }
+}
