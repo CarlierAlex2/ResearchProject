d77d7461374819739a0e46ad802fe86cdff6e37a
CarlierAlex2 <alex.carlier@gmail.com>
2022-01-27 01:05:49 +0100

    PPO - compare VELOCITY_MIN config

--- Assets/Scripts/Config/Config_6.cs
+++ Assets/Scripts/Config/Config_6.cs
@@ -104,3 +104,56 @@ public class ConfigReward_6_2 : ConfigReward
         this.STEERING_ANGLE = 0.01f;
     }
 }
+
+//-------------------------------------------------------------------
+
+
+public class ConfigAgent_6_3 : ConfigAgent
+{
+    public ConfigAgent_6_3() : base()
+    {
+        this.INDEX_START = 1;
+        //--
+        this.CHECKPOINT_OFFSET = 0;
+        this.CHECKPOINT_RANGE = 2.3f;
+
+        this.CHECKPOINT_ANGLE = 20f;
+        this.CHECKPOINT_ANGLE_MAX = 120f;
+
+        this.CHECKPOINT_DIST = 5f;
+        this.CHECKPOINT_DIST_MAX = 6.5f;
+        //--
+        this.VELOCITY_MIN = 0.1f;
+        this.STEERING_ANGLE = 30f;
+        this.STEERING_ANGLE_MIN = 15f;
+    }
+}
+
+public class ConfigReward_6_3 : ConfigReward
+{
+    public ConfigReward_6_3() : base()
+    {
+        this.TIME = -0.01f;
+        this.FORWARD = 0.005f;
+        this.BREAK = -0.05f;
+        this.GOAL = 0;
+        //--
+        this.WALL_ENTER = -5f;
+        this.WALL_STAY = 0;
+        //--
+        this.CHECKPOINT_RANGE = 0.5f;
+        this.CHECKPOINT_PASS = 0;
+
+        this.CHECKPOINT_DIST = 0.1f;
+        this.CHECKPOINT_DIST_NEG = -0.05f;
+        this.CHECKPOINT_DIST_MAX = -10f;
+
+        this.CHECKPOINT_ANGLE = 0.05f; //0.01f;
+        this.CHECKPOINT_ANGLE_NEG = 0.05f;//-0.01f;
+        this.CHECKPOINT_ANGLE_MAX = -10f;
+        //--
+        this.VELOCITY_MIN = 1f;
+        this.VELOCITY_FORWARD = -0.3f;
+        this.STEERING_ANGLE = 0.01f;
+    }
+}
