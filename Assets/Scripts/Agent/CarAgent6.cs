71b853873727dd5448253d5be6b666855ee7a801
CarlierAlex2 <alex.carlier@gmail.com>
2022-01-31 16:18:59 +0100

    Cleanup code - CarAgent

--- Assets/Scripts/Agent/CarAgent6.cs
+++ Assets/Scripts/Agent/CarAgent6.cs
@@ -6,58 +6,38 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
 
+/// <summary> 
+/// Defines the agent's behavior.
+/// <para>This class inherits from the agent class from the Unity ML-Agents Toolkit</para> 
+/// <para>Github: https://github.com/Unity-Technologies/ml-agents</para> 
+/// </summary>
 public class CarAgent6 : Agent
 {
-    //mlagents-learn config/PPO_CarAgent6.yaml --run-id=PPO_CarAgent6_4_128 --env=builds/PPO_CarAgent6
-    //mlagents-learn config/PPO_CarAgent6_b256.yaml --run-id=PPO_CarAgent6_4_256 --env=builds/PPO_CarAgent6 
-    //mlagents-learn config/PPO_CarAgent6_b512.yaml --run-id=PPO_CarAgent6_4_512 --env=builds/PPO_CarAgent6 
-
-    //mlagents-learn config/PPO_CarAgent6_n32.yaml --run-id=PPO_CarAgent6_4_n32 --env=builds/PPO_CarAgent6
-    //mlagents-learn config/PPO_CarAgent6_n64.yaml --run-id=PPO_CarAgent6_4_n64 --env=builds/PPO_CarAgent6 
-    //mlagents-learn config/PPO_CarAgent6_n128.yaml --run-id=PPO_CarAgent6_4_n128 --env=builds/PPO_CarAgent6 
-
-    //mlagents-learn config/SAC_CarAgent6.yaml --run-id=SAC_CarAgent6 --env=builds/SAC_CarAgent6 
-
-    //mlagents-learn config/SAC_CarAgent6_test4.yaml --run-id=SAC_CarAgent6_test4 --env=builds/SAC_CarAgent6 
-    //mlagents-learn config/SAC_CarAgent6_test5.yaml --run-id=SAC_CarAgent6_test5 --env=builds/SAC_CarAgent6 
-
-    //mlagents-learn config/PPO/PPO_CarAgent6.yaml --run-id=PPO_CarAgent6_5_test1 --env=builds/PPO_CarAgent6
-
-    //mlagents-learn config/SAC/SAC_CarAgent6_b1024_n64_s20.yaml --run-id=SAC_CarAgent6_5_b1024_n64_s20 --env=builds/SAC_CarAgent6
-
-    //mlagents-learn config/PPO/PPO_CarAgent6_5_buff5120.yaml --run-id=PPO_CarAgent6_5_buff5120_noAngle --env=builds/PPO_CarAgent6
-
-    //mlagents-learn config/PPO/PPO_CarAgent6_batchsize.yaml --run-id=PPO_6_5_angle_120_none --env=builds/PPO_CarAgent6_multi
-    //mlagents-learn config/PPO/PPO_CarAgent6_batchsize.yaml --run-id=PPO_6_5_angle_120_neg --env=builds/PPO_CarAgent6_multi_2
-
-    //mlagents-learn config/PPO/PPO_CarAgent6_default.yaml --run-id=PPO_6_5_testVelocity_2 --env=builds/PPO_CarAgent6
-
-    //mlagents-learn config/SAC/SAC_stepsUpdateBatch3.yaml --run-id=SAC_stepsUpdateBatch3 --env=builds/SAC_CarAgent6_multi --resume
-    
-    //mlagents-learn config/SAC/SAC_03_entcoef.yaml --run-id=SAC_03_entcoef --env=builds/SAC_CarAgent6_multi
-    //mlagents-learn config/SAC/SAC_04_learningRate.yaml --run-id=SAC_04_learningRate --env=builds/SAC_CarAgent6_multi
-
-    //mlagents-learn config/PPO/PPO_03_gamma.yaml --run-id=PPO_03_gamma --env=builds/PPO_CarAgent6_multi
-
+    // --- External objects ---
     [SerializeField] private Transform target;
     [SerializeField] private GPS pathfinding;
     [SerializeField] private EnvController envController;
     [SerializeField] private TextMesh debugTextMesh;
 
-    //---
+
+    // --- Configuration ---
     private static ConfigAgent CONFIG = new ConfigAgent_6_4();
     private static ConfigReward REWARDS = new ConfigReward_6_4();
     private static RewardFunctions REWARD_FUNCTIONS = new RewardFunctions(CONFIG, REWARDS);
 
-    //---
+
+    // --- Components ---
     private Rigidbody rigid;
     private CarAgentWheel wheelController;
 
-    //---
+
+    // --- Pathfinding ---
     private int index = 0;
     private List<Vector3> path;
+    private bool isCheckPoint = false;
 
-    //---
+
+    // --- Observation ---
     private Vector3 positionOld;
     private Vector3 positionNew;
     private Vector3 forwardOld;
@@ -65,17 +45,23 @@ public class CarAgent6 : Agent
     private Vector3 checkpoint;
     private Vector3 velocityOld;
     private Vector3 velocityNew;
-
-    //---
-    private bool isEpisodeRunning = false;
-    private bool isCheckPoint = false;
-    private bool stopEpisode = false;
      
+
+    // --- Actions ---
     private float [] actionOutput = {0, 0, 0};
     private int [] actionSpace = {7, 7, 3};
 
 
+    // --- Episode ---
+    private bool isEpisodeRunning = false;
+    private bool stopEpisode = false;
+
+
     //--- INITIALIZE ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is executed at the start of the training. 
+    // <para>Overriden from the Agent class from the Unity ML-Agents Toolkit.</para> 
+    /// </summary>
     public override void Initialize()
     {
         Debug.Log("Initialize Agent");
@@ -83,19 +69,32 @@ public class CarAgent6 : Agent
         wheelController = GetComponent<CarAgentWheel>();
     }
 
+    /// <summary> 
+    /// This method is executed at the start of each episode. 
+    // <para>Overriden from the Agent class from the Unity ML-Agents Toolkit.</para> 
+    /// </summary>
     public override void OnEpisodeBegin()
     {
-        envController.InitEnvironment(); //watch out for infinite loop, Initialize or OnEpisodeBegin first?
+        // initialize & reset environment
+        envController.InitEnvironment();
         envController.ResetEnvironment(this.transform);
+
+        // reset agent
         wheelController.ResetVelocity();
         ResetAgent();
     }
 
+    /// <summary> 
+    /// This method resets the agent variables for a new episode. 
+    /// </summary>
     private void ResetAgent()
     {
+        //pathfinding
         index = 0;
         path = null;
+        isCheckPoint = false;
 
+        //observations
         positionOld = Vector3.zero;
         positionNew = Vector3.zero;
         forwardOld = Vector3.zero;
@@ -104,15 +103,19 @@ public class CarAgent6 : Agent
         velocityNew = Vector3.zero;
         checkpoint = Vector3.zero;
 
+        //episode
         stopEpisode = false;
         isEpisodeRunning = true;
-        isCheckPoint = false;
-        Debug.Log("Reset Agent");
     }
 
     //--- OBSERVATION ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is used to collect observations for the agent and is executed each step. 
+    // <para>Overriden from the Agent class from the Unity ML-Agents Toolkit.</para> 
+    /// </summary>
     public override void CollectObservations(VectorSensor sensor)
     {
+        // check agent observations and reward when episode is running
         if(isEpisodeRunning)
         {
             UpdatePath();   //Pathfinding
@@ -120,11 +123,13 @@ public class CarAgent6 : Agent
             RewardCar();    //Rewards
         }
 
+        // get directions to checkpoint (including vector magnitude)
         Vector3 directionOld = checkpoint - positionOld;
         Vector3 directionNew = checkpoint - positionNew;
         directionOld.y = 0;
         directionNew.y = 0;
 
+        // add observations
         sensor.AddObservation(directionOld); 
         sensor.AddObservation(directionNew);
 
@@ -140,77 +145,89 @@ public class CarAgent6 : Agent
     }
 
     //--- INFO ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is retrieves the agent's current state for observations. 
+    /// </summary>
     private void GetAgentInfo()
     {
+        // agent position
         positionOld = positionNew;
         positionNew = transform.position;
 
+        // agent forward vector
         forwardOld = forwardNew;
         forwardNew = transform.forward;
 
+        // agent velocity
         velocityOld = velocityNew;
         velocityNew = rigid.velocity;
     }
 
 
     //--- ACTIONS ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is used to manually control the agent using input keys. 
+    // <para>Overriden from the Agent class from the Unity ML-Agents Toolkit.</para> 
+    /// </summary>
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        // Input keys
-        float actionMove, actionRotate;
-        int actionBrake;
-
+        // input keys
         float move = Input.GetAxisRaw("Vertical");
         float rotate = Input.GetAxisRaw("Horizontal");
         bool isBraking = Input.GetKey(KeyCode.Space);
 
-        // [-1, 0, +1] => [0, 1, 2] *3 => [0, 3, 6]                 
-        actionMove = (move + 1f) * ((float)(actionSpace[0]-1) / 2f);    
-        actionRotate = (rotate + 1f) * ((float)(actionSpace[1]-1) / 2f); 
+        // convert input keys to actions (same range as output of model)               
+        float actionMove = (move + 1f) * ((float)(actionSpace[0]-1) / 2f);    
+        float actionRotate = (rotate + 1f) * ((float)(actionSpace[1]-1) / 2f); 
+        int actionBrake = (isBraking) ? (actionSpace[2]-1) : 0;       
 
-        // [0, +1] => [0, 2]
-        actionBrake = (isBraking) ? (actionSpace[2]-1) : 0;       
-
-        // Continous
+        // set actions
         ActionSegment<int> actionArr = actionsOut.DiscreteActions;
         actionArr[0] = (int)actionMove;
         actionArr[1] = (int)actionRotate;
         actionArr[2] = (int)actionBrake;
     }
 
+    /// <summary> 
+    /// This method is used to execute actions when receiving output from the AI-model. 
+    // <para>Overriden from the Agent class from the Unity ML-Agents Toolkit.</para> 
+    /// </summary>
     public override void OnActionReceived(ActionBuffers actions)
     {      
-        //--
+        // check if episode is running & path is set (cannot act without path)
         if(!isEpisodeRunning || path == null)
             return;
 
         //--
         if (isEpisodeRunning)
         {
-            //Finish
+            // stop episode when signal is given
             if (stopEpisode) 
                 FinishEpisode();
 
-            //Actions
+            // get action output
             float move = (float)actions.DiscreteActions[0];
             float rotate = (float)actions.DiscreteActions[1];
             float isBraking = (float)actions.DiscreteActions[2];
 
+            // convert output to agent actions
             move = move / ((float)(actionSpace[0]-1) / 2f);
             move -= 1f;         
-            rotate = rotate / ((float)(actionSpace[1]-1) / 2f);    // [0, 3, 6] => [0, 1, 2] => [-1, 0, +1]
+            rotate = rotate / ((float)(actionSpace[1]-1) / 2f);
             rotate -= 1f;     
-            isBraking = isBraking / ((float)actionSpace[2]-1f);    // [0, 2] => [0, 1]
+            isBraking = isBraking / ((float)actionSpace[2]-1f);
 
-            //Debug
+            // store actions
             actionOutput[0] = move;
             actionOutput[1] = rotate;
             actionOutput[2] = isBraking;
+
+            // set wheelcontroller steering
             wheelController.SetActions(move, rotate, isBraking);
         }
         else
         {
-            // Debug
+            // no actions if episode is over
             actionOutput[0] = 0;
             actionOutput[1] = 0;
             actionOutput[2] = 0;
@@ -219,63 +236,85 @@ public class CarAgent6 : Agent
 
 
     //--- REWARDS ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is calculates reward signals for the agent based on its state. 
+    /// </summary>
     private void RewardCar()
     {
-        float reward = 0f;
-
         //--
+        float reward = 0f;
         Vector3 localVelocity = transform.InverseTransformDirection(velocityNew);
 
+        // calculate rewards
         reward += REWARD_FUNCTIONS.Time();
         reward += REWARD_FUNCTIONS.Movement(localVelocity.z, actionOutput);
         reward += REWARD_FUNCTIONS.Checkpoint(isCheckPoint);
         reward += REWARD_FUNCTIONS.CheckpointDist(positionOld, positionNew, checkpoint);
         reward += REWARD_FUNCTIONS.CheckpointAngle(positionOld, forwardOld, positionNew, forwardNew, checkpoint);
 
-        //--
+        // set reward for agent's model
         AddReward(reward);
 
-        //--
-        isCheckPoint = false;
+        // check for maximum negative reward to stop the episode
         float minReward = -10f;
         stopEpisode = (reward <= minReward) ? true : stopEpisode;
+
+        // setup next step
+        isCheckPoint = false;
     }
 
 
     //--- PATH ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is manages the agent's pathfinding. 
+    /// </summary>
     private void UpdatePath()
     {
-        if(path == null) // check if path exists
+        // check if path exists
+        if(path == null) 
             SetPath();
-        if(path == null) // still doesn't exist => no direction
-            return;
-        checkpoint = path[index];
 
-        //--
+        // if path still doesn't exist => no directions
+        if(path == null) 
+            return;
+
+        // update current checkpoint
+        checkpoint = path[index];
         Vector3 checkpointVector = checkpoint - (this.transform.position + transform.forward * CONFIG.CHECKPOINT_OFFSET);
         checkpointVector.y = 0;
+
+        // when close enough to checkpoint, increase index to proceed to next one
         if(checkpointVector.magnitude <= CONFIG.CHECKPOINT_RANGE)
         {
+            // increment index of path
             index++;
+
+            // check if endgoal is reached
             stopEpisode = (index >= path.Count) ? true : false;
 
+            // update when checkpoint is reached
             index = (index > path.Count - 1) ? (path.Count - 1) : index;
             checkpoint = path[index];
-
             isCheckPoint = true;
-            Debug.Log("Update path, index=" + index);
+            //Debug.Log("Update path, index=" + index);
         }
     }
 
+    /// <summary> 
+    /// This method is requests a path from the GPS based on the endgoal and the agent's position. 
+    /// </summary>
     private void SetPath()
     {
         path = pathfinding.FindPath(this.transform.position, target.position);
         index = CONFIG.INDEX_START;
-        Debug.Log("Path calculated");
+        //Debug.Log("Path calculated");
     }
 
 
     //--- COLLISIONS ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is executed when a trigger object is entered. 
+    /// </summary>
     private void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Wall")
@@ -286,11 +325,17 @@ public class CarAgent6 : Agent
         }
     }
 
+    /// <summary> 
+    /// This method is executed when collision started with an object. 
+    /// </summary>
     private void OnCollisionEnter(Collision collision) 
     {
 
     }
 
+    /// <summary> 
+    /// This method is executed when collision keeps happening with an object. 
+    /// </summary>
     private void OnCollisionStay(Collision collision) 
     {
 
@@ -298,32 +343,50 @@ public class CarAgent6 : Agent
 
 
     //--- FINISH ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method is executed to finish an episode and prevent other code from running. 
+    /// </summary>
+
     private void FinishEpisode()
     {
         isEpisodeRunning = false;
         Debug.Log("End episode!");
-        EndEpisode(); // calls CollectObservations => don't nest
+        EndEpisode(); 
+        // NOTE: EndEpisode calls CollectObservations. Avoid nesting cause this results in an infinite loop
     }
 
     //--- DEBUG ---------------------------------------------------------------------
+    /// <summary> 
+    /// This method updated debug information for the agent displayed when running. 
+    /// </summary>
     private void DebugObservation()
     {
-        Vector3 directionNew = checkpoint - positionNew;
-        Vector3 velocity = rigid.velocity;
-
+        // agent position + offset (to be able to see lines)
         Vector3 c = transform.position + Vector3.up * 1f;
+
+        // draw lines for checkpoint
+        Vector3 directionNew = checkpoint - positionNew;
         Debug.DrawLine(c, c + directionNew, Color.red);
+
+        // draw lines for velocity
+        Vector3 velocity = rigid.velocity;
         Debug.DrawLine(c, c + velocity, Color.blue);
 
+        // update text
         debugTextMesh.text = actionOutput[0] + "\n" + actionOutput[1] + "\n" + actionOutput[2];
     }
 
+    /// <summary> 
+    /// This method is shows debug information for the pathfinding. 
+    /// </summary>
     private void DebugPath()
     {
+        // draw path
         Vector3 c = Vector3.up * 1f;
         for(int i = 0; i < path.Count-1; i++)
             Debug.DrawLine(c + path[i], c + path[i+1], Color.green, 1f);
 
+        // draw line to goal
         Vector3 toGoal = (path[path.Count - 1] -this.transform.position);
         toGoal.y = 0;
         c += transform.position;
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ResearchProject
+# ResearchProject - Self Driving AI with route
 Research Project for MCT
+
+# How to run training
+Example commands
+> mlagents-learn <path_config> --run-id=<run_id> --env=<path_build>
+
+> mlagents-learn config/SAC/SAC_01_batchSize.yaml --run-id=SAC_01_batchSize --env=builds/SAC_CarAgent6_multi
+
+> mlagents-learn config/PPO/PPO_01_beta.yaml --run-id=PPO_01_beta --env=builds/PPO_CarAgent6_multi
